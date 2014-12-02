aa85c2504e741e3c11bc5baf6d1e74cce83d23f0
root <root@jalmes-P5GC-MX-1333.(none)>
2014-12-03 00:01:38 +0800

    with search and delte command

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-echo "RESULTS"
-find /home/jalmes/Documents/test/ -mtime -2 
+log=/home/jalmes/Documents/logs/del_log
+echo "Deleted files -->"
+find /home/jalmes/Documents/test/ -type f -atime -2 -delete -print >"$log" 
+cat /home/jalmes/Documents/logs/del_log
