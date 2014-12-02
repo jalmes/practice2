f2ee47bd92544772f710f73fa263023e533b61fd
root <root@jalmes-P5GC-MX-1333.(none)>
2014-12-03 00:07:47 +0800

    latest

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 log=/home/jalmes/Documents/logs/del_log
-echo "Deleted files -->"
+echo "Deleted files"
 find /home/jalmes/Documents/test/ -type f -atime -2 -delete -print >"$log" 
 cat /home/jalmes/Documents/logs/del_log
